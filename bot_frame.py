8237c302a019e7a2df20d2d9fad8dabfd337568b
PenutChen <penut85420@gmail.com>
2020-06-02 06:40:01 +0800

    Add bot frame

---
+++ bot_frame.py
@@ -0,0 +1,16 @@
+import os
+import random
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(aliases=['抽籤'])
+async def draw(ctx):
+    fortune = ['大吉', '吉', '小吉', '小凶', '凶', '大凶']
+    rtn = random.choice(fortune)
+
+    await ctx.channel.send(rtn)
+
+if __name__ == '__main__':
+    token = os.environ['TOKEN']
+    bot.run(token)
